004ad7b4a51031d3bea05eb1192b5aac2506876d
Gullit Miranda <gullitmiranda@requestdev.com.br>
2012-11-08 21:30:24 -0400

    update rdcms v0.7.3 - bug relacionamento produtos-uploads;

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,12 +7,12 @@ class Product < ActiveRecord::Base
   # Relacionando com os uploads
   has_and_belongs_to_many :uploads,
     :join_table => "products_uploads",
-    :order => "position asc",
-    :insert_sql =>  proc { |record|
-      # created_at = record.created_at == "0000-00-00 00:00:00" ? Time.now : record.created_at
-      %{INSERT INTO `products_uploads` (`product_id`, `upload_id`, `position`) VALUES ( "#{self.id}","#{record.id}", "#{Time.now.to_i}" )}
-      # %{INSERT INTO `products_uploads` (`product_id`, `upload_id`, `position`, `created_at`, `updated_at`) VALUES ( "#{self.id}","#{record.id}", "#{Time.now.to_i}", "#{created_at}", "#{Time.now}" )}
-    }
+    :order => "position asc"#,
+    # :insert_sql =>  proc { |record|
+    #   # created_at = record.created_at == "0000-00-00 00:00:00" ? Time.now : record.created_at
+    #   %{INSERT INTO `products_uploads` (`product_id`, `upload_id`, `position`) VALUES ( "#{self.id}", "#{record.id}", "#{Time.now.to_i}" )}
+    #   # %{INSERT INTO `products_uploads` (`product_id`, `upload_id`, `position`, `created_at`, `updated_at`) VALUES ( "#{self.id}","#{record.id}", "#{Time.now.to_i}", "#{created_at}", "#{Time.now}" )}
+    # }
   accepts_nested_attributes_for :uploads
 
   # attr_accessible :images
