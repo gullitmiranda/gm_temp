c279a8aa5095f0e0f4a2af566c070560333f7d4b
Gullit Miranda <gullitmiranda@requestdev.com.br>
2013-02-11 17:47:10 -0400

    remove test/dummy

--- test/dummy/spec/models/visitor_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-# has_many :role_users, :as => :operator
-# has_many :roles, :through => :role_users
-
-describe Visitor do
-  it {should have_many(:roles).through(:role_users)}
-end
