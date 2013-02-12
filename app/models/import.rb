91dd9b2cf89f6354c677f72761cf05a8c4c01cd2
Gullit Miranda <gullitmiranda@requestdev.com.br>
2013-02-11 22:11:58 -0400

    model updates: help, import and location;

--- app/models/import.rb
+++ app/models/import.rb
@@ -2,52 +2,51 @@
 #
 # Table name: imports
 #
-#  id           :integer(4)      not null, primary key
+#  id           :integer          not null, primary key
 #  assignment   :text
 #  target_model :string(255)
-#  successful   :boolean(1)
-#  upload_id    :integer(4)
-#  separator    :string(255)     default(",")
+#  successful   :boolean
+#  upload_id    :integer
+#  separator    :string(255)      default(",")
 #  result       :text
-#  created_at   :datetime        not null
-#  updated_at   :datetime        not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 
 class Import < ActiveRecord::Base
   require "csv"
-  attr_accessible :target_model, :upload_id, :separator
   belongs_to :upload, :class_name => Upload
   serialize :assignment
-  
+
   BlockedAttributes = ["id", "created_at", "updated_at", "url_name", "slug"]
-  
+
   def analyze_csv
     result = []
     data = CSV.read(self.upload.image.path, {:col_sep => self.separator})
     data.first.each_with_index do |a, index|
       result << [a,index.to_s]
-    end 
+    end
     @analyze_csv ||= result
   end
-  
+
   def get_model_attributes
     @get_model_attributes ||= eval("#{self.target_model}.new.attributes").delete_if{|a| BlockedAttributes.include?(a) }.keys
   end
-  
-  def get_association_names 
+
+  def get_association_names
     self.target_model.constantize.reflect_on_all_associations.collect { |r| r.name }
   end
-  
+
   def method_missing(meth, *args, &block)
     if meth.to_s.include?("assignment_") && self.assignment.present?
       self.assignment[meth.to_s.split("_")[1]]
     end
-  end   
-  
+  end
+
   def status
     @status ||="ready to import"
   end
-  
+
   def run!
     self.result = []
     count = 0
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@
 #
 # Table name: locations
 #
-#  id         :integer(4)      not null, primary key
+#  id         :integer          not null, primary key
 #  lat        :string(255)
 #  lng        :string(255)
 #  street     :string(255)
@@ -10,8 +10,8 @@
 #  zip        :string(255)
 #  region     :string(255)
 #  country    :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #  title      :string(255)
 #
 
@@ -19,14 +19,15 @@ class Location < ActiveRecord::Base
   geocoded_by :complete_location, :latitude  => :lat, :longitude => :lng
   after_validation :geocode
   liquid_methods :street, :city, :zip, :region, :country, :title
-  
-  def complete_location 
+  belongs_to :locateable, :polymorphic => true
+
+  def complete_location
     result = ""
     result += "#{self.street}" if self.street.present?
     result += ", #{self.zip}" if self.zip.present?
     result += ", #{self.city}" if self.city.present?
   end
-  
+
   def title
     self.complete_location
   end
