fbc0d3eab5642dba1b7a761cda4a944e11899ea9
Gullit Miranda <gullitmiranda@requestdev.com.br>
2013-02-11 21:37:24 -0400

    article cache worker update

--- app/worker/articles_cache_worker.rb
+++ app/worker/articles_cache_worker.rb
@@ -3,9 +3,17 @@ class ArticlesCacheWorker
   sidekiq_options queue: "default"
 
   def perform
+    #cache leeren
     Article.active.each do |article|
       article.updated_at = Time.now
       article.save
+    end
+    #Alte Versionen von has_paper_trail löschen (https://github.com/airblade/paper_trail)
+    if ActiveRecord::Base.connection.table_exists?("settings")
+      if Setting.for_key("rdcms.remove_old_versions.active") == "true"
+        weekcount = Setting.for_key("rdcms.remove_old_versions.weeks").to_i
+        Version.delete_all ["created_at < ?", weekcount.weeks.ago]
+      end
     end
   end
 
