abd8693dbdafe4919ccf3e379166a032ce050318
Gullit Miranda <gullitmiranda@requestdev.com.br>
2013-04-01 17:09:08 -0400

    slider update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,24 +1,25 @@
 class MoviesController < ApplicationController
   load_and_authorize_resource
-  
+
   respond_to :html, :json
 
   def index
     if request.xhr?
       @movies = get_video_list
-      
+
       respond_with @movies do |format|
         format.html { render :layout => false }
       end
     end
   end
+
   def render_list
     @movies = get_video_list
     respond_with @movies
   end
 
   private
-  
+
   def get_video_list(start=1, limit=nil)
     object = eval("Youtube::#{Setting.for_key("rdcms.videos.object").classify}")
     data = object.find(
